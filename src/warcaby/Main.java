e989c2e8a29991f321f04142f9ade3e0a4f35bab
Mykhailo Shkurupii <smysyv@gmail.com>
2026-06-15 11:24:02 +0200

    Add Application Entry Point

---
+++ src/warcaby/Main.java
@@ -0,0 +1,13 @@
+package warcaby;
+
+import warcaby.gui.MainFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+}
